259e706511bc29c8e68f362d42b939137488979b
barba <barba@DESKTOP-D016LKB>
2025-11-09 17:14:35 -0400

    Creacion de clase paciente

--- ProyectoFinalICC104/src/logico/Paciente.java
+++ ProyectoFinalICC104/src/logico/Paciente.java
@@ -1,15 +1,5 @@
 package logico;
 
-<<<<<<< HEAD
 public class Paciente extends Persona {
-=======
-public class Paciente {
-	
-	private String id;
-	private String nombre;
-	private String apellido;
-	private String probando;
-
->>>>>>> branch 'master' of https://github.com/TriunfelMoises/Proyecto_Final_POO.git
 
 }
